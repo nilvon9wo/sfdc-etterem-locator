b93a61233d733609de3c2ab13c7bba77f24a7e3f
Brian Kessler <kessler.bm@gmail.com>
2017-03-25 20:14:39 +0100

    Struggling with Restaurant Locator

---
+++ src/aura/InTheArea/InTheAreaController.js
@@ -0,0 +1,23 @@
+({ 
+	doInit : function(component, event, helper) {
+		var main = component.find('main');
+		$A.util.removeClass(main, 'small');
+		$A.util.addClass(main, component.get('v.designHeight'));
+
+		if (component.get('v.recordId')) {
+			helper.getLocationList(component, helper);	 
+		} else {
+			helper.positionStyleEnhancements(component);
+			helper.setPosition(component, helper);
+		}
+	},
+    
+    showDetails: function(component, event, helper) {
+		helper.closeItem(component.get('v.openItem'));
+		helper.openItem(component, event.currentTarget);
+	},    
+    
+	updateSearch : function(component, event, helper) {
+		helper.getLocationList(component, helper);
+	}    
+})
---
+++ src/aura/InTheArea/InTheAreaHelper.js
@@ -0,0 +1,107 @@
+({
+	closeItem: function(currentItem) {
+		if (currentItem) {
+			var itemDetails = currentItem.querySelector('[data-details]');
+			$A.util.addClass(itemDetails, 'slds-hide');
+		}
+	}, 
+
+    doLayout: function(component, response) {
+    	$A.util.addClass(component.find('spinner'), 'slds-hide');
+
+        var warning = component.find('warning');
+        var responseValue = (JSON.parse(response.getReturnValue()));
+        if (responseValue.error) { 
+    		component.set('v.errorMessage', responseValue.error);
+    		$A.util.removeClass(warning, 'slds-hide');    	
+        } else {
+	        var restaurantList = responseValue.bizArray;
+        	$A.util.addClass(warning, 'slds-hide');
+        	component.set('v.restaurantList', restaurantList);
+        }
+    },
+
+    getLocationList: function(component, helper) {
+		/*
+    	$A.util.removeClass(component.find('spinner'), 'slds-hide');
+    	var searchTerm = helper.selectSearchTerm(component);
+
+		console.log('##### hello!');
+		var recordId = null; // component.get('v.recordId');
+    	console.log('####### component.get(v.recordId)', recordId);
+
+        var action = component.get('v.recordId')
+			? helper.getLocationListForRecord(component, searchTerm)
+			: helper.getLocationListForPosition(component, searchTerm);
+		*/			
+
+		/*
+        action.setCallback(this, function(response) {
+            this.doLayout(component, response);
+        });
+
+        $A.enqueueAction(action);
+        */
+    },
+
+    getLocationListForPosition: function(component, searchTerm) {
+    	var coordinates = component.get("v.location").coords;
+        var action = component.get('c.getListByGeoCoordinates');
+       	action.setParams({
+       		latitude = coordinates.latitude,
+       		longitude = coordinates.longitude,
+            searchTerm: searchTerm
+        });
+		return action;
+    },
+    
+    getLocationListForRecord: function(component, searchTerm) {
+    /*
+    	console.log('############ getLocationListForRecord');
+        var action = null; //component.get('c.getListForRecord');
+    	console.log('####### action', action);
+    	var params = {
+            sObjectType: component.get('v.sObjectName'),
+            recordId: component.get('v.recordId'),
+            searchTerm: searchTerm
+        }
+    	console.log('####### params', params);
+       	action.setParams(params);
+		return action;
+	*/		    	
+    }, 
+
+    openItem: function(component, selectedItem) {
+		component.set('v.openItem', selectedItem);
+		var itemDetails = selectedItem.querySelector('[data-details]');
+		$A.util.removeClass(itemDetails, 'slds-hide');
+    },
+
+    positionStyleEnhancements: function(component) {
+			$A.util.addClass(component.find('main'), 'autoHeight');
+			var scrollableArea = component.find('scrollableArea');
+			$A.util.removeClass(scrollableArea, 'scroll-container');
+			$A.util.removeClass(scrollableArea, 'slds-scrollable-y');
+    },
+    
+    selectSearchTerm: function (component) {
+        var searchTerm = component.find('searchTerm').get('v.value');
+        return searchTerm
+        	? searchTerm
+        	: component.get('v.defaultSearch');
+    },
+
+	setPosition: function(component, helper) {
+		$A.util.removeClass(component.find('spinner'), 'slds-hide');
+		navigator.geolocation.getCurrentPosition(handleCurrentPosition, handleError);
+		
+		function handleCurrentPosition(location) {
+			component.set('v.location', location);
+		}
+		
+		function handleError() {
+			component.set('v.errorMessage', 'Could not get your current geolocation.');
+			$A.util.removeClass(component.find('warning'), 'slds-hide');
+		}
+	}
+})
