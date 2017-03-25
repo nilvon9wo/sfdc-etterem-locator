b6322febab37e445ed72f2730184ff6ceefb84c7
Brian Kessler <kessler.bm@gmail.com>
2017-03-25 20:41:29 +0100

    controller reinstated, but no helpers so still no function

--- src/aura/InTheArea/InTheAreaController.js
+++ src/aura/InTheArea/InTheAreaController.js
@@ -1,7 +1,5 @@
 ({
 	doInit : function(component, event, helper) {
-		console.log('#### DIE PAINFULLY!');
-	/*
 		var main = component.find('main');
 		$A.util.removeClass(main, 'small');
 		$A.util.addClass(main, component.get('v.designHeight'));
@@ -12,19 +10,14 @@
 			helper.positionStyleEnhancements(component);
 			helper.setPosition(component, helper);
 		}
-	*/		
 	},
     
     showDetails: function(component, event, helper) {
-	/*    
 		helper.closeItem(component.get('v.openItem'));
 		helper.openItem(component, event.currentTarget);
-	*/		
 	},    
     
 	updateSearch : function(component, event, helper) {
-	/*
 		helper.getLocationList(component, helper);
-	*/		
 	}    
 })
